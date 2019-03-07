1f5a2fa0298cbad8f0ebf217c077e0d6b753195f
Yoav Nordmann <Yoav.Nordmann@cyren.com>
2019-03-07 22:31:51 +0200

    Fix deletion of existing older tags

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: Watch Category
 # about: Watches a category for all the users in a particular group
-# version: 0.5
+# version: 0.6
 # authors: Arpit Jalan
 # maintainer: Yoav Nordmann
 # url: https://github.com/tikalk/discourse-watch-category
@@ -76,17 +76,21 @@ module ::WatchCategory
 
   def self.change_tag_notification_pref_for_group(group_tags, pref)
     group_tags.each do |group_name, tags|
-      tags ||= []
-      group = Group.find_by_name(group_name)
 
-      unless tags.empty? || group.nil?
-        if group_name == "everyone"
-          User.all.each do |user|
-            TagUser.batch_set(user, pref, tags)
-          end
-        else
-          group.users.each do |user|
-            TagUser.batch_set(user, pref, tags)
+      tags.each do |tag_name|
+
+        tag = Tag.find_by_name(tag_name)
+        group = Group.find_by_name(group_name)
+
+        unless tag.nil? || group.nil?
+          if group_name == "everyone"
+            User.all.each do |user|
+              TagUser.change(user.id, tag.id, pref)
+            end
+          else
+            group.users.each do |user|
+              TagUser.change(user.id, tag.id, pref)
+            end
           end
         end
       end
